b2bb58bb55146bba9bd80fe6c779f574fc451bab
rhenpunzel <rhencelleannnbernal21@gmail.com>
2026-07-28 09:43:34 +0800

    done with error

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,123 @@
-@extends('errors::minimal')
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 | PAGE NOT FOUND </title>
+
+    <style>
+       * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Roboto', sans-serif;
+}
+
+.wrap {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.error-icon {
+    margin-bottom: 10px;
+}
+
+.error-icon img {
+    width: 450px;
+    height: auto;
+    display: block;
+}
+
+.content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.5rem;
+    text-align: center;
+}
+
+
+.content h3 {
+    border-bottom: 3px solid yellow;
+    font-size: 24px;
+    font-weight: bold;
+}
+
+.content p {
+    font-size: 20px;
+    font-weight: 600;
+}
+
+
+.content span {
+    font-size: 10px;
+    color: #666;
+    font-weight: 500;
+}
+.access-denied {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+
+    padding: 6px 16px;
+    margin: 4px 0;
+
+    border: 1.5px solid #1f4a9d;
+    border-radius: 25px;
+
+    background: #fff;
+}
+
+.access-denied .lock {
+    font-size: 18px;
+    color: #f4b400;
+}
+
+.access-denied .text {
+    font-size: 14px;
+    font-weight: 600;
+    color: #b22222;
+    letter-spacing: 1px;
+}
+
+.system-name {
+    font-size: 10px;
+    color: #666;
+    
+}
+    </style>
+</head>
+
+<body>
+
+    <div class="wrap">
+
+        <div class="error-icon">
+           <img src="/images/errorlogo.png" alt="ERROR LOGO">
+        </div>
+
+        <div class="content">
+            <h3>404 | PAGE NOT FOUND </h3>
+            <p> The requested page or resource could not be found.</p>
+
+</div>
+<span class="system-name">
+            <span>TUPAD | Tulong Panghanapbuhay sa ating Disadvantaged Workers</span>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -1,5 +1,122 @@
-@extends('errors::minimal')
 
-@section('title', __('Too Many Requests'))
-@section('code', '429')
-@section('message', __('Too Many Requests'))
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>429 | TOO MANY REQUEST</title>
+
+    <style>
+       * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Roboto', sans-serif;
+}
+
+.wrap {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.error-icon {
+    margin-bottom: 10px;
+}
+
+.error-icon img {
+    width: 450px;
+    height: auto;
+    display: block;
+}
+
+.content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.5rem;
+    text-align: center;
+}
+
+
+.content h3 {
+    border-bottom: 3px solid yellow;
+    font-size: 24px;
+    font-weight: bold;
+}
+
+.content p {
+    font-size: 20px;
+    font-weight: 600;
+}
+
+
+.content span {
+    font-size: 10px;
+    color: #666;
+    font-weight: 500;
+}
+.access-denied {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+
+    padding: 6px 16px;
+    margin: 4px 0;
+
+    border: 1.5px solid #1f4a9d;
+    border-radius: 25px;
+
+    background: #fff;
+}
+
+.access-denied .lock {
+    font-size: 18px;
+    color: #f4b400;
+}
+
+.access-denied .text {
+    font-size: 14px;
+    font-weight: 600;
+    color: #b22222;
+    letter-spacing: 1px;
+}
+
+.system-name {
+    font-size: 10px;
+    color: #666;
+}
+    </style>
+</head>
+
+<body>
+
+    <div class="wrap">
+
+        <div class="error-icon">
+           <img src="/images/errorlogo.png" alt="ERROR LOGO">
+        </div>
+
+        <div class="content">
+            <h3>429 | TOO MANY REQUEST </h3>
+            <p>You have made too many requests in a short period.</p>
+
+</div>
+<span class="system-name">
+            <span>TUPAD | Tulong Panghanapbuhay sa ating Disadvantaged Workers</span>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
